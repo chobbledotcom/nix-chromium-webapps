fcea949b06fa8e054237b4826cbb92b52b5e2255
Stefan <git@stefn.co.uk>
2025-09-26 16:47:27 +0100

    Don't force unstable nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,7 @@
 {
   description = "NixOS module for running web applications as Chromium desktop apps";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: {
+  outputs = { self }: {
     nixosModules.default = import ./module.nix;
     nixosModules.chromium-webapps = import ./module.nix;
   };
