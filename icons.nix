18370b8d36cbf1c002f51d72ca04bd115265d47c
Stefan <git@stefn.co.uk>
2025-09-26 18:09:58 +0100

    Fix transparent backgrounds

--- icons.nix
+++ icons.nix
@@ -8,7 +8,7 @@ pkgs.runCommand "${app.name}-icons" { nativeBuildInputs = [ pkgs.imagemagick ]; 
   mkdir -p $out/share/icons/hicolor
   for size in 16 24 32 48 64 96 128 256 512; do
     mkdir -p $out/share/icons/hicolor/''${size}x''${size}/apps
-    magick ${app.icon} -background none -resize ''${size}x''${size} \
+    magick ${app.icon} -background none -alpha on -resize ''${size}x''${size} \
       $out/share/icons/hicolor/''${size}x''${size}/apps/${iconName}.png
   done
 ''
